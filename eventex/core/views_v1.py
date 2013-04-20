0b4f80cf4544ed96ed3bee8ac3092bd94c1af5b0
tarcilo <tarcilodiogo@gmail.com>
2013-04-20 17:08:51 -0300

    All your base are belong to us

---
+++ eventex/core/views_v1.py
@@ -0,0 +1,12 @@
+# Create your views here.
+# coding: utf-8
+from django.http import HttpResponse
+from django.template import loader, Context
+
+def homepage(request):
+	t = loader.get_template('index.html')
+	c = Context()
+
+	content = t.render(c)
+
+	return HttpResponse(content)
